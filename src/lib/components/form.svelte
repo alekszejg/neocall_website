b54f6a71ea14e4e2b8e792c631ef399e134f1be8
Alekszej Guljajev <alexeyguljajev@gmail.com>
2025-08-22 19:21:13 +0200

    Downloaded and formatted form component, prob to be used on every page. Currently only html + visuals, not functioning

---
+++ src/lib/components/form.svelte
@@ -0,0 +1,47 @@
+<section class="lpv3 finance-contact" id="finance-contact">
+    <div class="finance-container">
+        <div class="finance-contact__inner">
+            <div class="finance-contact__form-wrapper">
+                <h2 class="title title--02">Learn more</h2>
+                <div class="finance-contact__description">
+                     <p>Leave your information below to find the right plan for your business and check for available promos</p>
+                </div>
+                <div class="finance-contact__form">    
+                    <div class="wpcf7 no-js" id="wpcf7-f6939-p1840-o1" lang="en-US" dir="ltr" data-wpcf7-id="6939">
+                        <div class="screen-reader-response">
+                            <p role="status" aria-live="polite" aria-atomic="true"></p> 
+                            <ul></ul>
+                        </div>
+                        <form action="/pricing/#wpcf7-f6939-p1840-o1" method="post" class="wpcf7-form init" aria-label="Contact form" novalidate="novalidate" data-status="init">
+                            <div style="display: none;">
+                                <input type="hidden" name="_wpcf7" value="6939">
+                                <input type="hidden" name="_wpcf7_version" value="6.0.1">
+                                <input type="hidden" name="_wpcf7_locale" value="en_US">
+                                <input type="hidden" name="_wpcf7_unit_tag" value="wpcf7-f6939-p1840-o1">
+                                <input type="hidden" name="_wpcf7_container_post" value="1840">
+                                <input type="hidden" name="_wpcf7_posted_data_hash" value="">
+                                <input type="hidden" name="_wpcf7_recaptcha_response" value="">
+                            </div>
+                            <div class="finance-contact__form-row">
+                                <span class="wpcf7-form-control-wrap" data-name="your-name"><input size="40" maxlength="400" class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required" autocomplete="name" aria-required="true" aria-invalid="false" placeholder="Name" value="" type="text" name="your-name"></span>
+                            </div>
+                            <div class="finance-contact__form-row">
+                                <span class="wpcf7-form-control-wrap" data-name="your-email"><input size="40" maxlength="400" class="wpcf7-form-control wpcf7-email wpcf7-validates-as-required wpcf7-text wpcf7-validates-as-email" autocomplete="email" aria-required="true" aria-invalid="false" placeholder="Email" value="" type="email" name="your-email"></span>
+                            </div>
+                            <div class="finance-contact__form-row">
+                                <span class="wpcf7-form-control-wrap" data-name="text-616"><input size="40" maxlength="400" class="wpcf7-form-control wpcf7-text" aria-invalid="false" placeholder="Website" value="" type="text" name="text-616"></span>
+                            </div>
+                            <div class="finance-contact__form-row finance-contact__form-company-row display-none">
+                                <span class="wpcf7-form-control-wrap" data-name="company"><input size="40" maxlength="400" class="wpcf7-form-control wpcf7-text" aria-invalid="false" placeholder="Company" value="" type="text" name="company"></span>
+                            </div>
+                            <div class="finance-contact__form-row">
+                                <input class="wpcf7-form-control wpcf7-submit has-spinner" type="submit" value="Learn more">
+                            </div>
+                            <div class="wpcf7-response-output" aria-hidden="true"></div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
