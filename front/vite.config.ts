d477c74d2e83c3cc906b16c1570dc7ad45dbe91c
Alekszej Guljajev <alexeyguljajev@gmail.com>
2025-08-24 08:07:51 +0200

    Added backend as proxy

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -3,6 +3,11 @@ import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+	server: {
+		proxy: {
+			"/api": "http://localhost:3000"
+		}
+	},
 	plugins: [
 		sveltekit(),
 		tailwindcss(),
