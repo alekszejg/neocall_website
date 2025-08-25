b60b6bf5fa5154cad770aba611d2557c19fb4083
Alekszej Guljajev <alexeyguljajev@gmail.com>
2025-08-26 00:30:34 +0200

    Added language button for changing translations + icons that it will use

---
+++ front/src/lib/components/langButton.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import { lang } from "$lib/stores/language";
+
+  function toggleLang() {
+    lang.update(current => (current === "en" ? "de" : "en"));
+  }
+</script>
+
+<button class="w-[2.5rem] h-[2rem] ml-2" on:click={toggleLang}>
+    <img class="h-full" 
+    src={$lang === "de" ? "/icons/flag-england.svg" : "/icons/flag-germany.svg"}
+    alt={$lang === "de" ? "Translate to english" : "Translate to german"}
+    />
+</button>
