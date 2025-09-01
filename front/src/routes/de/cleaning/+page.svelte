43ea23e0bb9efd728220a350aef4249aaa15e98b
Alekszej Guljajev <alexeyguljajev@gmail.com>
2025-09-01 07:32:51 +0200

    Added more blank pages for product's industries. Proper update - after 1st deploy

---
+++ front/src/routes/de/cleaning/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+    import Navbar from '$lib/components/navbar.svelte';
+    import Form from '$lib/components/form.svelte';
+    import Footer from '$lib/components/footer.svelte';
+    import { lang } from '$lib/stores/language';
+    
+    lang.set("de")
+</script>
+
+
+
+<Navbar />
+<Form />
+<Footer />
