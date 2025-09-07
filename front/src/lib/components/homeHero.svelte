903c8ddb11eb1105dbf9a8b025e75b88e993498d
Alekszej Guljajev <alexeyguljajev@gmail.com>
2025-09-07 04:24:36 +0200

    Localized homepage hero, finished styling it

--- front/src/lib/components/homeHero.svelte
+++ front/src/lib/components/homeHero.svelte
@@ -1,8 +1,13 @@
+<script lang="ts">
+  import { lang } from '$lib/stores/language';
+  import localization from '$lib/localizations/homepage.json';
+</script>
+
 <div id="hpHeroContainer" class="flex flex-col items-center">
-    <p id="hpHeader1" class="w-[200px] text-center border-2 homepageP">Your AI Assistent</p>
-    <h1 id="hpHeader2" class="w-3/4 text-center leading-normal">Supportkosten um bis zu 80% reduzieren</h1>
-    <p class="w-3/4 text-center homepageP">smao ist dein cloud-basierter KI-Telefonassistent für sofortige, automatisierte Anrufbearbeitung -  24/7, DSGVO-konform, DE-gehostet und nahtlos in dein CRM integriert.</p>
+    <p id="hpHeader1" class="w-[250px] text-center border-2 homepageP">{localization.hero.header1[$lang]}</p>
+    <h1 id="hpHeader2" class="w-3/4 text-center leading-normal">{localization.hero.header2[$lang]}</h1>
+    <p class="w-3/4 text-center homepageP">{localization.hero.text[$lang]}</p>
     <div id="hpLinkDiv" class="w-[250px] text-center border-2">
-        <a href="#finance-contact" class="no-underline">Jetzt konstelos testen</a>
+        <a href="#finance-contact" class="block no-underline">{localization.hero.testNow[$lang]}</a>
     </div>
 </div>
