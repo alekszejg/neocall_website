d7cc65d886e1046968710c930df7dba3ace32ed3
Alekszej Guljajev <alexeyguljajev@gmail.com>
2025-09-09 20:04:26 +0200

    Added new localization for button, as alternative to 'BOOK A DEMO' text

--- front/src/lib/components/homeHero.svelte
+++ front/src/lib/components/homeHero.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
   import { lang } from '$lib/stores/language';
   import localization from '$lib/localizations/homepage.json';
+  import localizationGlobal from '$lib/localizations/global.json';
 </script>
 
 <div id="hpHeroContainer" class="flex flex-col items-center">
     <p id="hpHeader1" class="w-[250px] text-center border-2 homepageP">{localization.hero.header1[$lang]}</p>
     <h1 id="hpHeader2" class="w-3/4 text-center leading-normal">{localization.hero.header2[$lang]}</h1>
     <p class="w-3/4 text-center homepageP">{localization.hero.text[$lang]}</p>
-    <div id="hpLinkDiv" class="w-[250px] text-center border-2">
-        <a href="#finance-contact" class="block no-underline">{localization.hero.testNow[$lang]}</a>
+    <div id="hpLinkDiv" class="w-[280px] text-center border-2">
+        <a href="#finance-contact" class="block no-underline">{localizationGlobal.testNow[$lang]}</a>
     </div>
 </div>
