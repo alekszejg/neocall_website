3aefc1312a39f3032ef6bbff70e9f91a526b34de
Alekszej Guljajev <alexeyguljajev@gmail.com>
2025-09-10 20:15:14 +0200

    All necessary client-side warnings/errors are displayed and translated to user. Overriden some styling

--- front/src/lib/components/form.svelte
+++ front/src/lib/components/form.svelte
@@ -1,60 +1,72 @@
 <script lang="ts">
+  import { onMount } from 'svelte';
   import { lang } from '$lib/stores/language';
   import localization from '$lib/localizations/navFooterForm.json';
+  import localizationGlobal from '$lib/localizations/global.json';
   
-  let name = "";
-  let email = "";
-  let comment = "";
-  let company = "";
-
-  $: nameErr = name.length > 50 ? localization.contactForm.errors.name[$lang] : "";
-  $: emailErr = email.length > 100 ? localization.contactForm.errors.email[$lang] : "";
-  $: commentErr = comment.length > 500 ? localization.contactForm.errors.comment[$lang] : "";
-
-  $: formValid = !nameErr && !emailErr && !commentErr && name && email && comment;
-
-  let loading = false;
-  let sent = false;
+  let formData = { name: "", email: "", comment: "", company: "" };
+  let formState = { submitted: false, loading: false, sent: false};
   let internalErr = false;
+  
+  // reset everything to defaults for each onMount
+  onMount(() => {
+    formData = { name: "", email: "", comment: "", company: "" };
+    formState = { submitted: false, loading: false, sent: false };
+    internalErr = false;
+  })
 
+  $: nameRequiredErr = formState.submitted && !formData.name;
+  $: nameLengthErr = formData.name.length > 50;
+
+  let emailFocused = false; // !emailFocused hides emailFormatErr while user types
+  $: emailRequiredErr = formState.submitted && !formData.email; // !emailRequiredErr below hides emailFormatErr for empty emails
+  $: emailFormatErr = formState.submitted && !emailFocused && !emailRequiredErr && !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(formData.email);
+  $: emailLengthErr = formData.email.length > 100;
+  let emailDomainErr = false;
+
+  $: commentLengthErr = formData.comment.length > 500;
+  
+  $: formValid = (
+    (!nameRequiredErr && !nameLengthErr) && 
+    (!emailRequiredErr && !emailFormatErr && !emailLengthErr) && !commentLengthErr);
+  
   async function submit(e: SubmitEvent) {
     e.preventDefault();
+    formState.submitted = true;
+
     if (!formValid) {
-      return; 
+        return; 
     }
     
-    internalErr = false;
-    loading = true;
-    
-    const payload = { name, email, comment, company };
+    formState.loading = true;
     const res = await fetch("/api/submit-form", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(payload)
+        body: JSON.stringify(formData)
     });
 
-    loading = false;
+    const data = await res.json();
+    formState.loading = false;
+
     if (res.ok) {
-        sent = true;
-        await new Promise(r => setTimeout(r, 1000)); 
-        sent = false;
-        name = "";
-        email = "";
-        comment = "";
-        company = "";
+        formState.sent = true;
+        await new Promise(r => setTimeout(r, 1500)); 
+        formState.sent = false;
+        formState.submitted = false;
+        formData = { name: "", email: "", comment: "", company: "" };
+    } else if (res.status === 400 && data.error === "EMAIL_DOMAIN_NOT_FOUND") {
+        emailDomainErr = true;
     } else {
         internalErr = true;
     }
     }
-
-
 </script>
 
 <section class="lpv3 finance-contact" id="finance-contact">
     <div class="finance-container">
         <div class="finance-contact__inner">
             <div class="finance-contact__form-wrapper">
-                <h2 class="title title--02">{localization.contactForm.learnMore[$lang]}</h2>
+                <h2 class="title title--02 text-nowrap">{localization.contactForm.learnMore[$lang]}</h2>
                 <div class="finance-contact__description">
                      <p>{localization.contactForm.description[$lang]}</p>
                 </div>
@@ -66,34 +78,65 @@
                         </div>
                         <form method="post" class="wpcf7-form init" aria-label="Contact form" on:submit={submit}>
                             <div class="finance-contact__form-row">
-                                <input size="40" autocomplete="name" placeholder={localization.contactForm.inputs.name[$lang]} bind:value={name} type="text" required>
-                                {#if nameErr}
-                                    <p class="formError">{nameErr}</p>
+                                <input 
+                                    size="40" 
+                                    autocomplete="name" 
+                                    placeholder={localization.contactForm.inputs.name[$lang]} 
+                                    class:red-border={nameRequiredErr} 
+                                    bind:value={formData.name} 
+                                    type="text" 
+                                />
+                                {#if nameRequiredErr}
+                                    <p class="formError">{localization.contactForm.errors.name.required[$lang]}</p>
+                                {/if}
+                                {#if nameLengthErr}
+                                    <p class="formError">{localization.contactForm.errors.name.length[$lang]}</p>
                                 {/if}
                             </div>
                             <div class="finance-contact__form-row">
-                                <input size="40" autocomplete="email" placeholder={localization.contactForm.inputs.email[$lang]} bind:value={email} type="email" required>
-                                {#if emailErr}
-                                    <p class="formError">{emailErr}</p>
+                                <input 
+                                    size="40" 
+                                    autocomplete="email" 
+                                    placeholder={localization.contactForm.inputs.email[$lang]} 
+                                    class:red-border={emailRequiredErr} 
+                                    bind:value={formData.email}
+                                    on:focus={() => {
+                                        emailFocused = true;
+                                        emailDomainErr = false;
+                                    }}
+                                    on:blur={() => emailFocused = false} 
+                                    type="email" 
+                                />
+                                {#if emailRequiredErr}
+                                    <p class="formError">{localization.contactForm.errors.email.required[$lang]}</p>
+                                {/if}
+                                {#if emailFormatErr}
+                                    <p class="formError">{localization.contactForm.errors.email.format[$lang]}</p>
+                                {/if}
+                                {#if emailLengthErr}
+                                    <p class="formError">{localization.contactForm.errors.email.length[$lang]}</p>
+                                {/if}
+                                {#if emailDomainErr}
+                                    <p class="formError">{localization.contactForm.errors.email.domain[$lang]}</p>
                                 {/if}
                             </div>
                             <div class="finance-contact__form-row">
-                                <textarea class="w-full" rows="6" placeholder={localization.contactForm.inputs.comment[$lang]} bind:value={comment} required></textarea>
-                                {#if commentErr}
-                                    <p class="formError">{commentErr}</p>
+                                <textarea class="w-full" rows="6" placeholder={localization.contactForm.inputs.comment[$lang]} bind:value={formData.comment}></textarea>
+                                {#if commentLengthErr}
+                                    <p class="formError">{localization.contactForm.errors.comment.length[$lang]}</p>
                                 {/if}
                             </div>
                             <div class="finance-contact__form-row finance-contact__form-company-row display-none">
-                                <input size="40" placeholder="Company" bind:value={company} type="text" name="company">
+                                <input size="40" placeholder="Company" bind:value={formData.company} type="text" name="company">
                             </div>
                             <div class="finance-contact__form-row">
                                 <input 
                                     class="wpcf7-form-control wpcf7-submit has-spinner" 
                                     type="submit" 
                                     value={
-                                        (!loading && !sent && localization.contactForm.btnText.learnMore[$lang]) || 
-                                        (loading && !sent && localization.contactForm.btnText.sending[$lang]) || 
-                                        (sent && localization.contactForm.btnText.sent[$lang])
+                                        (!formState.loading && !formState.sent && localizationGlobal.testNow[$lang]) ||
+                                        (formState.loading && !formState.sent && localization.contactForm.btnText.sending[$lang]) ||
+                                        (formState.sent && localization.contactForm.btnText.sent[$lang])
                                     }>
                                 {#if internalErr}
                                     <p class="formError">{localization.contactForm.errors.internalErr[$lang]}</p>
